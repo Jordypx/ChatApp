860f16cc6aeca6ef0548efe4de5593265c30df22
Audrey <audreypx100@gmail.com>
2022-07-17 20:01:10 +0100

    my_chatapp

---
+++ client/src/components/chat.vue
@@ -0,0 +1,47 @@
+<template>
+
+<div>
+  <preloader/>
+</div>
+
+<div>
+  <chatbody/>
+</div>
+
+</template>
+
+
+
+
+
+<script>
+import preloader from '../components/preloader.vue'
+import chatbody from '../components/chatbody.vue'
+
+
+export default {
+   name: "chat",
+
+   components: {
+    preloader,
+    chatbody,
+   }
+
+}
+
+
+
+
+</script>
+
+
+
+
+
+
+
+<style>
+
+
+
+</style>
---
+++ client/src/components/preloader.vue
@@ -0,0 +1,42 @@
+<template>
+<transition>
+   <div v-if="show" class="flex flex-col items-center justify-center absolute w-full h-full bg-white z-9999 ease-out duration-300">
+       <div class="mb-24"><img class="w-56" src="../assets/whatsapp-logo-png-2259.png" alt="whatsapp"></div>
+   </div>
+</transition>
+
+<div></div>
+
+</template>
+
+
+
+
+<script>
+export default {
+   name: 'preloader',
+   data() {
+      return {
+         show: true
+      }
+   },
+   mounted(){
+     this.showToggle()
+   },
+   methods: {
+      showToggle(){
+         setTimeout(() => {
+               this.show = false
+         }, 6000)
+      }
+   }
+   
+}
+</script>
+
+
+
+<style scoped>
+
+
+</style>
